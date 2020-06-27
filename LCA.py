9c45e20d49df5f350ba2d44a38123c69cace949e
shannonzchanz <34292916+shannonzchanz@users.noreply.github.com>
2020-06-27 17:06:28 +0800

    Can do for multiple analysis

--- LCA.py
+++ LCA.py
@@ -33,59 +33,65 @@ soap_ad = 0.00075
 electricity_ad = 0.01875
 dishwashing = {"yes": water_ef * water_ad + soap_ef * soap_ad + electricity_ef * electricity_ad, "no": 0}
 
-# configuration variables
-# ====
-people_per_HH = 3
-dist_per_HH = 3
-HH = 6
-inter_cluster_dist = 6
-# ===
+
+def analysis(people_per_HH, dist_per_HH, HH, inter_cluster_dist):
+
+    # analysis
+    matrix = np.zeros((7, 6),) # matrix initialization
+
+    for no_of_HH in range(1, HH+1):
+        # as no of HH increases, total household distance increases
+        total_household_dist = dist_per_HH * no_of_HH * 2
+        # as no of HH increases, delivery distance still remains the same
+
+        total_delivery_dist = dist_per_HH * 2 + inter_cluster_dist
+        fd_gmotor_plastic_disposablecutleries = emission_models(plastic["yes"], cutleries["yes"], container["yes"], no_of_HH,
+                                                                people_per_HH, dishwashing["no"],
+                                                                transport["gasoline motorcycle"], total_delivery_dist)
+
+        fd_gmotor_noplastic_disposablecutleries = emission_models(plastic["no"], cutleries["yes"], container["yes"], no_of_HH,
+                                                                  people_per_HH, dishwashing["no"],
+                                                                  transport["gasoline motorcycle"], total_delivery_dist)
+
+        fd_gmotor_plastic_nocutleries = emission_models(plastic["yes"], cutleries["no"], container["yes"], no_of_HH,
+                                                        people_per_HH, dishwashing["no"],
+                                                        transport["gasoline motorcycle"], total_delivery_dist)
+
+        fd_emotor_noplastic_disposablecutleries = emission_models(plastic["no"], cutleries["yes"], container["yes"], no_of_HH,
+                                                                  people_per_HH, dishwashing["no"],
+                                                                  transport["electric motorcycle"], total_delivery_dist)
+
+        d_gcar_nondisposablecutleries = emission_models(plastic["no"], cutleries["no"], container["no"], no_of_HH,
+                                                        people_per_HH, dishwashing["yes"],
+                                                        transport["gasoline car"], total_household_dist)
+
+        d_ecar_nondisposablecutleries = emission_models(plastic["no"], cutleries["no"], container["no"], no_of_HH,
+                                                        people_per_HH, dishwashing["yes"],
+                                                        transport["electric car"], total_household_dist)
+
+        d_gbus_nondisposablecutleries = emission_models(plastic["no"], cutleries["no"], container["no"], no_of_HH,
+                                                        people_per_HH, dishwashing["yes"],
+                                                        transport["gasoline bus"], total_household_dist * people_per_HH)
+
+        matrix[0][no_of_HH-1] = fd_gmotor_plastic_disposablecutleries.total_emission
+        matrix[1][no_of_HH-1] = fd_gmotor_noplastic_disposablecutleries.total_emission
+        matrix[2][no_of_HH-1] = fd_gmotor_plastic_nocutleries.total_emission
+        matrix[3][no_of_HH-1] = fd_emotor_noplastic_disposablecutleries.total_emission
+        matrix[4][no_of_HH-1] = d_gcar_nondisposablecutleries.total_emission
+        matrix[5][no_of_HH-1] = d_ecar_nondisposablecutleries.total_emission
+        matrix[6][no_of_HH-1] = d_gbus_nondisposablecutleries.total_emission
+
+    print (matrix)
+
+# analysis(people_per_HH, dist_per_HH, HH, inter_cluster_dist)
+# same cluster, 3km away
+analysis(3, 3, 6, 0)
+# same cluster, 10km away
+analysis(3, 10, 6, 0)
+# different cluster, 3km away, inter-cluster 1.5km
+analysis(3, 3, 6, 1.5)
+# different cluster, 3km away, inter-cluster 6km
+analysis(3, 3, 6, 6)
 
 
-# analysis
-matrix = np.zeros((7, 6),) # matrix initialization
-
-for no_of_HH in range(1, HH+1):
-    # as no of HH increases, total household distance increases
-    total_household_dist = dist_per_HH * no_of_HH * 2
-    # as no of HH increases, delivery distance still remains the same
-
-    total_delivery_dist = dist_per_HH * 2 + inter_cluster_dist
-    fd_gmotor_plastic_disposablecutleries = emission_models(plastic["yes"], cutleries["yes"], container["yes"], no_of_HH,
-                                                            people_per_HH, dishwashing["no"],
-                                                            transport["gasoline motorcycle"], total_delivery_dist)
-
-    fd_gmotor_noplastic_disposablecutleries = emission_models(plastic["no"], cutleries["yes"], container["yes"], no_of_HH,
-                                                              people_per_HH, dishwashing["no"],
-                                                              transport["gasoline motorcycle"], total_delivery_dist)
-
-    fd_gmotor_plastic_nocutleries = emission_models(plastic["yes"], cutleries["no"], container["yes"], no_of_HH,
-                                                    people_per_HH, dishwashing["no"],
-                                                    transport["gasoline motorcycle"], total_delivery_dist)
-
-    fd_emotor_noplastic_disposablecutleries = emission_models(plastic["no"], cutleries["yes"], container["yes"], no_of_HH,
-                                                              people_per_HH, dishwashing["no"],
-                                                              transport["electric motorcycle"], total_delivery_dist)
-
-    d_gcar_nondisposablecutleries = emission_models(plastic["no"], cutleries["no"], container["no"], no_of_HH,
-                                                    people_per_HH, dishwashing["yes"],
-                                                    transport["gasoline car"], total_household_dist)
-
-    d_ecar_nondisposablecutleries = emission_models(plastic["no"], cutleries["no"], container["no"], no_of_HH,
-                                                    people_per_HH, dishwashing["yes"],
-                                                    transport["electric car"], total_household_dist)
-
-    d_gbus_nondisposablecutleries = emission_models(plastic["no"], cutleries["no"], container["no"], no_of_HH,
-                                                    people_per_HH, dishwashing["yes"],
-                                                    transport["gasoline bus"], total_household_dist * people_per_HH)
-
-    matrix[0][no_of_HH-1] = fd_gmotor_plastic_disposablecutleries.total_emission
-    matrix[1][no_of_HH-1] = fd_gmotor_noplastic_disposablecutleries.total_emission
-    matrix[2][no_of_HH-1] = fd_gmotor_plastic_nocutleries.total_emission
-    matrix[3][no_of_HH-1] = fd_emotor_noplastic_disposablecutleries.total_emission
-    matrix[4][no_of_HH-1] = d_gcar_nondisposablecutleries.total_emission
-    matrix[5][no_of_HH-1] = d_ecar_nondisposablecutleries.total_emission
-    matrix[6][no_of_HH-1] = d_gbus_nondisposablecutleries.total_emission
-
-print (matrix)
 
